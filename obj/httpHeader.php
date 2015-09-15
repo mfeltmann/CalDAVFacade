519b64588dd67bc67766b1a49e487a1246bb4adc
mfeltmann <feltmann@eosnewmedia.de>
2015-09-15 18:54:16 +0200

    Implemented OPTIONS request handling

--- obj/httpHeader.php
+++ obj/httpHeader.php
@@ -4,27 +4,44 @@ class HttpHeader {
     
     function sendHeader( $requestMethod, $queryString ) {
         
-        if( $queryString == '' ) {
+        
+        
+        if( $queryString != '' ) {
 
-            self::_send_ok_header();
+            self::_send_not_found_header();
             return;
         }
 
-        self::_send_not_found_header();
+        if( $requestMethod == options_request ) {
+            
+            self::_send_options_header();
+            return;
+        }
+        
+        self::_send_ok_header();
         return;
     }
-    
-    function _send_ok_header() {
 
-        header( "HTTP/1.1 200 OK", false, 200 );
-        header( "Content-Length: 0" );
+    function _send_options_header() {
+    
+        header( 'HTTP/1.1 200 OK', false, 200 );
+        header( 'Allow: HEAD,OPTIONS' );
+        header( 'Content-Length: 0' );
     }
+    
 
     function _send_not_found_header() {
     
-        header( "HTTP/1.1 404 NOT FOUND", false, 404 );
-        header( "Content-Length: 0" );
+        header( 'HTTP/1.1 404 NOT FOUND', false, 404 );
+        header( 'Content-Length: 0' );
     }
+
+    function _send_ok_header() {
+
+        header( 'HTTP/1.1 200 OK', false, 200 );
+        header( 'Content-Length: 0' );
+    }
+
 }
 
 ?>
