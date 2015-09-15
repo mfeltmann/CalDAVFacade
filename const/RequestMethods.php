e087e4179d948f20e7682b29cff407180bbc4caf
mfeltmann <feltmann@eosnewmedia.de>
2015-09-15 18:53:43 +0200

    Prepared needed Request Methods

--- const/RequestMethods.php
+++ const/RequestMethods.php
@@ -1,7 +1,25 @@
 <?php
 
+// HTTP/1.1 Request Methods Section
+
+// Informational Request Methods
 define( 'head_request', 'HEAD' );
 define( 'options_request', 'OPTIONS' );
+
+// Manipulate Request Methods
+define( 'get_method', 'GET' );
+define( 'post_method', 'POST' );
+define( 'put_method', 'PUT' );
+define( 'delete_method', 'DELETE' );
+
+// Premature Reserverd Request Method
+define( 'connect_request', 'CONNECT' );
+
+// Unallowed Methods By Both Test And Productive Server
+define( 'trace_request', 'TRACE' );
+
+// End Of HTTP/1.1 
+
 
 
 ?>
