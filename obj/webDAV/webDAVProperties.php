bfb2910ae8e952eba55303f0fe33500fe6cf8a34
mfeltmann <feltmann@eosnewmedia.de>
2015-09-24 22:16:16 +0200

    Added WebDAV element functions

---
+++ obj/webDAV/webDAVProperties.php
@@ -0,0 +1,126 @@
+<?php
+
+    class WebDAVXMLElements {
+        
+        public function activelockElement() {
+            
+        }
+        
+        public function depthElement() {
+            
+        }
+        
+        public function locktokenElement() {
+            
+        }
+        
+        public function timeoutElement() {
+            
+        }
+        
+        public function collectionElement() {
+            
+        }
+        
+        public function hrefElement() {
+            
+        }
+        
+        public function linkElement() {
+            
+        }
+        
+        public function dstElement() {
+            
+        }
+        
+        public function srcElement() {
+            
+        }
+        
+        public function lockentryElement() {
+            
+        }
+        
+        public function lockinfoElement() {
+            
+        }
+        
+        public function lockscopeElement() {
+            
+        }
+        
+        public function exclusiveElement() {
+            
+        }
+        
+        public function sharedElement() {
+            
+        }
+        
+        public function locktypeElement() {
+            
+        }
+        
+        public function writeElement() {
+            
+        }
+        
+        public function multistatusElement() {
+            
+        }
+        
+        public function responseElement() {
+            
+        }
+        
+        public function responsedescriptionElement() {
+            
+        }
+        
+        public function ownerElement() {
+            
+        }
+        
+        public function propElement() {
+            
+        }
+        
+        public function propertybehaviorElement() {
+            
+        }
+        
+        public function keepaliveElement() {
+            
+        }
+        
+        public function omitElement() {
+            
+        }
+        
+        public function propertyupdateElement() {
+            
+        }
+        
+        public function removeElement() {
+            
+        }
+        
+        public function setElement() {
+            
+        }
+        
+        public function propfindElement() {
+            
+        }
+        
+        public function allpropElement() {
+            
+        }
+        
+        public function propnameElement() {
+            
+        }
+    }
+ 
+?>
